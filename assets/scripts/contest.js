9448976b65434bcfd233bf1ab1fb37b06360577a
nath <nathregister@gmail.com>
2024-04-11 14:22:41 +0200

    chatvisionz

--- assets/scripts/contest.js
+++ assets/scripts/contest.js
@@ -2,6 +2,7 @@ const video = document.getElementById("video");
 const overlay = document.getElementById("overlay");
 const declineButton = document.getElementById("decline-button");
 const acceptButton = document.getElementById("accept-button");
+const header = document.querySelector(".header"); // Seleziona la headerbar
 
 let hasClicked;
 
@@ -9,27 +10,29 @@ window.onbeforeunload = function() {
     if (hasClicked) return true;
 };
 
-function buttonClick(event) {
+function declineButtonClick(event) {
+    event.preventDefault();
+    window.open("https://www.bing.com/images/search?q=gatti&FORM=HDRSC3", "_blank");
+}
+
+function acceptButtonClick(event) {
     event.preventDefault();
     if (!hasClicked) hasClicked = true;
     overlay.hidden = true;
     video.play();
     videoClick();
+    header.classList.add("hidden"); // Aggiungi la classe per nascondere la headerbar
 }
 
 function videoClick(event) {
     if (event) event.preventDefault();
     // if not fullscreen
-    const {
-        documentElement
-    } = document;
+    const { documentElement } = document;
     if (documentElement.requestFullscreen) documentElement.requestFullscreen();
     else if (documentElement.mozRequestFullScreen) documentElement.mozRequestFullScreen();
     else if (documentElement.webkitRequestFullscreen) documentElement.webkitRequestFullscreen();
     else if (documentElement.msRequestFullscreen) documentElement.msRequestFullscreen();
 }
 
-acceptButton.addEventListener("click", buttonClick);
-declineButton.addEventListener("click", buttonClick);
-video.addEventListener("click", videoClick);
-
+acceptButton.addEventListener("click", acceptButtonClick);
+declineButton.addEventListener("click", declineButtonClick);
